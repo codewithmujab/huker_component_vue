48f7c96ef65159db0b563318e1fcba4fc1821e36
codewithmujab <saefulmujab300@gmail.com>
2023-12-14 16:26:59 +0700

    first commit component

---
+++ VBreadcrumb/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { router } from "@inertiajs/vue3";
+import { array } from 'vue-types';
+
+const props = defineProps({
+    routes: array()
+})
+
+const redirectToPage = (route, index) => {
+    if (index !== props.routes.length - 1) {
+        router.visit(route.to)
+    }
+}
+</script>
+
+<template>
+    <div>
+        <ul class="inline-flex flex-wrap text-sm font-medium sm:mb-6 mb-4">
+            <li class="after:content-['/'] last:after:hidden after:text-slate-400 after:px-2"
+                v-for="(route, index) in routes" :key="index">
+                <a :id="route.active ? 'active' : 'passive'" class="hover:text-primary cursor-pointer"
+                    :class="route.active ? 'text-primary' : 'text-slate-500'" @click="redirectToPage(route, index)">{{
+                        route.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
